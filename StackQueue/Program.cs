1dfc370aa8316d87f0576185a653156f35b15204
BashaVali <bashavali222@gmail.com>
2022-12-16 10:59:07 +0530

    Stack And Queue Problem

--- StackQueue/Program.cs
+++ StackQueue/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,24 +11,38 @@ namespace StackQueue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("-------------StackQueue-------------");
-            Console.WriteLine(" 1.Push ");
-            Console.WriteLine("Enter the option");
-            int num = Convert.ToInt32(Console.ReadLine());
-            Stack stack = new Stack();
-            switch (num)
+            Console.WriteLine("-------------Welcome To Stack Problem-------------");
+            string flag = "Y";
+            while (flag == "Y" || flag == "y")
             {
-                case 1:
-                    stack.Push(70);
-                    stack.Push(30);
-                    stack.Push(56);
-                    stack.Display();
-                    break;
-                default:
-                    Console.WriteLine("Enter a valid option");
-                    break;
+                Console.WriteLine("1.Push ");
+                Console.WriteLine("2.Peek And Pop ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                Stack stack = new Stack();
+                switch (num)
+                {
+                    case 1:
+                        stack.Push(70);
+                        stack.Push(30);
+                        stack.Push(56);
+                        stack.Display();
+                        break;
+                    case 2:
+                        stack.Push(70);
+                        stack.Push(30);
+                        stack.Push(56);
+                        stack.IsEmpty();
+                        stack.Pop();
+                        stack.Peek();
+                        stack.Display();
+                        break;
+
+                    default:
+                        Console.WriteLine("Enter a Valid Option!!!");
+                        break;
+                }
             }
-            Console.ReadLine();
+            Console.ReadKey();
         }
     }
 }
--- StackQueue/Stack.cs
+++ StackQueue/Stack.cs
@@ -6,33 +6,63 @@ using System.Threading.Tasks;
 
 namespace StackQueue
 {
-    class Stack
-    {
-        //Creating node a top
-        public Node top;
-        public void Push(int newData)
+        class Stack
         {
-            Node newNode = new Node(newData);
-            if (this.top != null)
+            //Creating node a top
+            public Node top;
+            public void Push(int newData)
             {
-                newNode.next = this.top;
+                Node newNode = new Node(newData);
+                if (this.top != null)
+                {
+                    newNode.next = this.top;
+                }
+                this.top = newNode;
+                Console.WriteLine("New Node {0} is added ", newNode.data);
             }
-            this.top = newNode;
-            Console.WriteLine("New Node {0} is added ", newNode.data);
-        }
-        public void Display()
-        {
-            Node temp = this.top;
-            if (temp == null)
+            //Peek method gives the top most element in stack without removing the element.
+            public void Peek()
             {
-                Console.WriteLine("No values to be added");
-                return;
+                if (this.top == null)
+                {
+                    Console.WriteLine("No elements present in the stack");
+                    return;
+                }
+                Console.WriteLine("The element after peeking is : " + this.top.data);
             }
-            while (temp != null)
+            //Pop Mthod-Removes the top most element
+            public void Pop()
             {
-                Console.WriteLine("Value in te stack " + temp.data);
-                temp = temp.next;
+                if (this.top == null)
+                {
+                    Console.WriteLine("No elements present in the stack");
+                    return;
+                }
+                Peek();
+                this.top = this.top.next;
+            }
+            //Checking the any underflow or overflow occurs
+            public void IsEmpty()
+            {
+                while (this.top != null)
+                {
+                    Peek();
+                    Pop();
+                }
+            }
+            public void Display()
+            {
+                Node temp = this.top;
+                if (temp == null)
+                {
+                    Console.WriteLine("No values to be added");
+                    return;
+                }
+                while (temp != null)
+                {
+                    Console.WriteLine("Value in the stack " + temp.data);
+                    temp = temp.next;
+                }
             }
         }
     }
-}
