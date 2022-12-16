061e559bccbc465effa173b24e4e95542e1a1550
BashaVali <bashavali222@gmail.com>
2022-12-16 11:16:37 +0530

    Stack And Queue Problem

--- StackQueue/Program.cs
+++ StackQueue/Program.cs
@@ -17,6 +17,8 @@ namespace StackQueue
             {
                 Console.WriteLine("1.Push ");
                 Console.WriteLine("2.Peek And Pop ");
+                Console.WriteLine("3.Queue ");
+                Console.WriteLine("Enter the option");
                 int num = Convert.ToInt32(Console.ReadLine());
                 Stack stack = new Stack();
                 switch (num)
@@ -37,12 +39,41 @@ namespace StackQueue
                         stack.Display();
                         break;
 
+                    case 3:
+                        string flag1 = "Y";
+                        while (flag1 == "Y" || flag1 == "y")
+                        {
+                            Console.WriteLine("1.Enqueue");
+                            Console.WriteLine("Enter the option");
+                            int num1 = Convert.ToInt32(Console.ReadLine());
+                            Queue queue = new Queue();
+                            switch (num1)
+                            {
+                                case 1:
+                                    queue.Enqueue(56);
+                                    queue.Enqueue(30);
+                                    queue.Enqueue(70);
+                                    queue.Display();
+                                    break;
+                                default:
+                                    Console.WriteLine("Enter a valid option");
+                                    break;
+                            }
+                            Console.WriteLine("\nDo you want to continue?(Y/N)");
+                            flag1 = Console.ReadLine();
+                        }
+                        break;
                     default:
                         Console.WriteLine("Enter a Valid Option!!!");
                         break;
                 }
+                Console.WriteLine("\nDo you want to continue?(Y/N)");
+                flag = Console.ReadLine();
             }
             Console.ReadKey();
+
+
         }
     }
 }
+         
